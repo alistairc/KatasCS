d9fdc05d2dab5e49e5fe392f1cbab9c203fe267f
Alistair Cooke <alistair.cooke77@gmail.com>
2023-07-03 15:10:51 +0100

    Retro-fitted some tests to cover the new endpoint
    Think it's better not to rely on the Acceptance test for behaviour

---
+++ Katas/BankChicago/Tests/AddingAndWithdrawingMoney.cs
@@ -0,0 +1,36 @@
+﻿using Katas.BankChicago.Tests.Infrastructure;
+
+namespace Katas.BankChicago.Tests;
+
+public class AddingAndWithdrawingMoney
+{
+    [Test]
+    public void Deposit_ShouldAddMoneyToTheAccount()
+    {
+        const decimal amountDeposited = 123.45m;
+        var depositDate = new DateOnly(2022, 12, 13);
+        
+        var account = new Account();
+        var endpoint = new BankAccountEndpoint(new FixedClock(depositDate), account);
+        endpoint.Deposit(amountDeposited);
+        
+        var statementLine = account.GetStatement().GetLines()[0];
+        statementLine.Amount.ShouldBe(amountDeposited);
+        statementLine.Date.ShouldBe(depositDate);
+    }
+
+    [Test]
+    public void Withdrawal_ShouldRemoveMoney()
+    {
+        const decimal amountWithdrawn = 123.45m;
+        var withdrawalDate = new DateOnly(2022, 12, 13);
+        
+        var account = new Account();
+        var endpoint = new BankAccountEndpoint(new FixedClock(withdrawalDate), account);
+        endpoint.Withdraw(amountWithdrawn);
+        
+        var statementLine = account.GetStatement().GetLines()[0];
+        statementLine.Amount.ShouldBe(0 - amountWithdrawn);
+        statementLine.Date.ShouldBe(withdrawalDate);
+    }
+}
