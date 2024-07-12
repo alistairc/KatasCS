0985b712bc3736a72f42ecebc80e7b910268e8d1
Alistair Cooke <alistair.cooke77@gmail.com>
2024-07-12 14:29:58 +0100

    Green acceptance test, all faked

---
+++ Katas/BankFakeIt/Tests/BankTestSystem.cs
@@ -0,0 +1,24 @@
+﻿namespace Katas.BankFakeIt.Tests;
+
+class BankTestSystem
+{
+    public BankTestSystem Deposit(TimestampedAmount timestampedAmount)
+    {
+        return this;
+    }
+
+    public BankTestSystem Withdraw(TimestampedAmount timestampedAmount)
+    {
+        return this;
+    }
+
+    public string GetStatementText()
+    {
+        return  """
+                Date       || Amount || Balance
+                14/01/2012 || -500   || 2500
+                13/01/2012 || 2000   || 3000
+                10/01/2012 || 1000   || 1000
+                """;
+    }
+}
