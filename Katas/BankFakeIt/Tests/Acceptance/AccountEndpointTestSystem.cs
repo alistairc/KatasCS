99c191a2e9d337ccd2f6d9f73da272679040e852
Alistair Cooke <alistair.cooke77@gmail.com>
2024-07-12 16:13:42 +0100

    Acceptance test, faked implementation

---
+++ Katas/BankFakeIt/Tests/Acceptance/AccountEndpointTestSystem.cs
@@ -0,0 +1,31 @@
+﻿namespace Katas.BankFakeIt.Tests.Acceptance;
+
+class AccountEndpointTestSystem
+{
+    public AccountEndpointTestSystem SetDate(DateOnly dateOnly)
+    {
+        return this;
+    }
+
+    public AccountEndpointTestSystem Deposit(decimal amount)
+    {
+        return this;
+    }
+
+    public AccountEndpointTestSystem Withdraw(decimal amount)
+    {
+        return this;
+    }
+
+    public string[] GetStatementText()
+    {
+        return
+        [
+            "Date        |     Deposit |  Withdrawal |     Balance",
+            "30/06/2022  |             |       49.50 |      950.50",
+            "29/06/2022  |    1,000.00 |             |    1,000.00",
+            "",
+            "Closing Balance: 950.50"
+        ];
+    }
+}
---
+++ Katas/BankFakeIt/Tests/Acceptance/StatementAfterTransactionsUserJourney.cs
@@ -0,0 +1,22 @@
+﻿namespace Katas.BankFakeIt.Tests.Acceptance;
+
+class StatementAfterTransactionsUserJourney
+{
+    [Test]
+    public void VerifyStatementAfterDepositAndWithdrawal()
+    {
+        var testSystem = new AccountEndpointTestSystem()
+            .SetDate(new DateOnly(2022, 06, 29))
+            .Deposit(1000)
+            .SetDate(new DateOnly(2022, 06, 30))
+            .Withdraw(49.50m);
+
+        //This seems very brittle, should be skip the formatting in this test?
+        var statement = testSystem.GetStatementText();
+        statement[0].ShouldBe("Date        |     Deposit |  Withdrawal |     Balance");
+        statement[1].ShouldBe("30/06/2022  |             |       49.50 |      950.50");
+        statement[2].ShouldBe("29/06/2022  |    1,000.00 |             |    1,000.00");
+        statement[3].ShouldBe("");
+        statement[4].ShouldBe("Closing Balance: 950.50");
+    }
+}
