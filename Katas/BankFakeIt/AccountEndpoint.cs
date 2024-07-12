333456a2d41ab0db99ca1a1e8f98b10ec6116453
Alistair Cooke <alistair.cooke77@gmail.com>
2024-07-12 16:33:53 +0100

    Extract Statement type

--- Katas/BankFakeIt/AccountEndpoint.cs
+++ Katas/BankFakeIt/AccountEndpoint.cs
@@ -14,13 +14,6 @@ class AccountEndpoint
 
     public IEnumerable<string> GetStatementText()
     {
-        return
-        [
-            "Date        |     Deposit |  Withdrawal |     Balance",
-            "30/06/2022  |             |       49.50 |      950.50",
-            "29/06/2022  |    1,000.00 |             |    1,000.00",
-            "",
-            "Closing Balance: 950.50"
-        ];
+        return new Statement().FormatAsText();
     }
 }
---
+++ Katas/BankFakeIt/Statement.cs
@@ -0,0 +1,16 @@
+﻿namespace Katas.BankFakeIt;
+
+public class Statement
+{
+    public IEnumerable<string> FormatAsText()
+    {
+        return
+        [
+            "Date        |     Deposit |  Withdrawal |     Balance",
+            "30/06/2022  |             |       49.50 |      950.50",
+            "29/06/2022  |    1,000.00 |             |    1,000.00",
+            "",
+            "Closing Balance: 950.50"
+        ];
+    }
+}
