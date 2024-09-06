d23a28c7820b537828291976f617b6153fbc9639
Alistair Cooke <alistair.cooke77@gmail.com>
2024-09-06 10:39:01 +0100

    A crappy main loop

---
+++ AntsWorld/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+
+var state = 0;
+while (true)
+{
+    var displayModel = MakeModel(state);
+    RenderModel(displayModel);
+    await Task.Delay(TimeSpan.FromMilliseconds(250));
+    state++;
+}
+return;
+
+void RenderModel(string[] strings)
+{
+    Console.SetCursorPosition(0, 0);
+    foreach (var line in strings)
+    {
+        Console.WriteLine(line);
+    }
+}
+
+string[] MakeModel(int state)
+{
+    const int displayHeight = 25;
+    const int displayWidth = 100;
+    var displayModel = new string[displayHeight];
+    var blankLine = new string(' ', displayWidth);
+    Array.Fill(displayModel, blankLine);
+    displayModel[state % displayHeight]=new string('X', displayWidth);
+    return displayModel;
+}
