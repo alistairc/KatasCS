6268daf1e1cac2d9d7002e1717e88d896e1320a3
Alistair Cooke <alistair.cooke77@gmail.com>
2024-09-06 16:34:31 +0100

    Add a couple of entities to the program

--- AntsWorld/Program.cs
+++ AntsWorld/Program.cs
@@ -4,7 +4,10 @@ const int worldWidth = 100;
 const int worldHeight = 25;
 
 var world = new World(worldWidth,worldHeight);
+
+world.Add(new BlockEntity(),  new WorldCoordinate(10,20));
 world.Add(new SingleDirectionAnt(Direction.Right), new WorldCoordinate(0,10));
+
 var projector = new WorldProjector(worldWidth, worldHeight);
 var game = new ConsoleGame();
 
